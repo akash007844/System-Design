d9febda5be62eef61a6a6b25c9865c5f04df93f1
Akash <akash007844@gmail.com>
2025-08-23 18:09:36 +0530

    [AKA]

--- src/main/java/com/example/system_design/MainDemo.java
+++ src/main/java/com/example/system_design/MainDemo.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SystemDesignApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SystemDesignApplication.class, args);
+		System.out.println("test");
 	}
 
 }
